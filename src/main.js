9b7a7b0f2dd3da1f9069fe69a7a8dc439bcc3128
Vinicius <vd.sevilha@gmail.com>
2021-10-10 21:41:39 -0300

    ADD: first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
+import Toast, { POSITION } from 'vue-toastification'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import 'animate.css'
+import '@/assets/css/tailwind.css'
+import '@/assets/css/fonts.css'
+import 'vue-toastification/dist/index.css'
+
+const app = createApp(App)
+app.use(router)
+app.use(Toast, { position: POSITION.BOTTOM_RIGHT })
+app.mount('#app')
